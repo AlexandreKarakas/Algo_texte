828da2efbcec9f14874e121a7eae73fb2d9b3ec5
AlexandreKarakas <alexandre.karakas@gmail.com>
2020-04-23 12:02:23 +0200

    ajout class IndexInverse

---
+++ src/IndexInverse.py
@@ -0,0 +1,20 @@
+class IndexInverse :
+    """Classe pour l'index inverse : 
+    -Identifiant
+    -Titre
+    -url de depart
+    -Contenu
+    -Urls de sortie
+    -Score """
+    
+    def __init__(self, ident, titre, urlDepart, contenu, list_url_sorti, score) :
+        """Constructeur IndexInverse"""
+        self.id = ident
+        self.titre = titre
+        self.urlD = urlDepart
+        self.contenu = contenu
+        self.list_url = list_url_sorti
+        self.score = score
+    
+    
+
