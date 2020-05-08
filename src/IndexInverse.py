742c06e5bb72ceaa62a9b1e2e6cf54442754a735
Davidu93000 <58073778+Davidu93000@users.noreply.github.com>
2020-05-08 22:52:31 +0200

    recherche mot similaire (proche)

--- src/IndexInverse.py
+++ src/IndexInverse.py
@@ -1,15 +1,13 @@
 class IndexInverse :
     """Classe pour l'index inverse : 
-    -Identifiant
     -Titre
     -url de depart
     -Contenu
     -Urls de sortie
     -Score """
     
-    def __init__(self, ident, titre, urlDepart, contenu, list_url_sorti, score) :
+    def __init__(self, titre, urlDepart, contenu, list_url_sorti, score) :
         """Constructeur IndexInverse"""
-        self.id = ident
         self.titre = titre
         self.urlD = urlDepart
         self.contenu = contenu
